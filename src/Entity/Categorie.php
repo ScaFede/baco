c0705ea662e76e2165185fbcadcecb58f813f76c
fede <federica@brixel.it>
2023-08-10 10:01:33 +0200

    New Entity Categorie, in relation with Competenze

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nome = null;
+
+    #[ORM\ManyToMany(targetEntity: CompetenzeBis::class, inversedBy: 'categorieRelation')]
+    private Collection $competenzeRelation;
+
+    public function __construct()
+    {
+        $this->competenzeRelation = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): static
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CompetenzeBis>
+     */
+    public function getCompetenzeRelation(): Collection
+    {
+        return $this->competenzeRelation;
+    }
+
+    public function addCompetenzeRelation(CompetenzeBis $competenzeRelation): static
+    {
+        if (!$this->competenzeRelation->contains($competenzeRelation)) {
+            $this->competenzeRelation->add($competenzeRelation);
+        }
+
+        return $this;
+    }
+
+    public function removeCompetenzeRelation(CompetenzeBis $competenzeRelation): static
+    {
+        $this->competenzeRelation->removeElement($competenzeRelation);
+
+        return $this;
+    }
+}
